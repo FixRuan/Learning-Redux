626417733700644f5830fe6bb8ec441b81a78e4a
fixruan <ruangoio01@gmail.com>
2022-12-23 12:54:35 -0300

    Types and dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import {connect} from "react-redux";
 
-function App() {
+function App({count, increment}) {
+
+  function handleOnClick(){
+    increment();
+  }
 
   return (
     <div>
       <h1>Hello World</h1>
+      <button onClick={handleOnClick}>Count is {count}</button>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    count: state.counter.value
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+    increment: () => dispatch({type: "counter/increment"})
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
